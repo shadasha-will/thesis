fd78e5ee616cf849049a1cf8ac1d36b770195e2c
shadasha-will <shadasha.williams90@gmail.com>
2021-07-22 23:52:19 +0200

    Create flair_ELMO.py

---
+++ flair_ELMO.py
@@ -0,0 +1,33 @@
+from typing import List
+
+from flair.data import Corpus
+from flair.datasets import ColumnCorpus
+from flair.embeddings import WordEmbeddings, TokenEmbeddings, CharacterEmbeddings, ELMoEmbeddings, StackedEmbeddings
+from flair.models import SequenceTagger
+from flair.trainers import ModelTrainer
+from flair.visual.training_curves import Plotter
+
+
+columns = {0: 'text', 1: 'ner'}
+data_folder = '/content/drive/MyDrive/thesis/training_data/'
+
+corpus: Corpus = ColumnCorpus(data_folder, columns, 
+                              train_file = 'data_gene_new_train.tsv',
+                              test_file = 'data_gene_new_test.tsv',
+                              dev_file = 'data_gene_new_valid.tsv')
+
+tag_type = 'ner'
+tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
+embedding_types : List[TokenEmbeddings] = [
+                                           WordEmbeddings('glove'),
+                                           CharacterEmbeddings(),
+                                           ELMoEmbeddings()]
+
+embeddings : StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
+tagger : SequenceTagger = SequenceTagger(hidden_size=256, embeddings=embeddings,
+                                         tag_dictionary=tag_dictionary,
+                                         tag_type=tag_type,
+                                         use_crf=True)
+trainer : ModelTrainer = ModelTrainer(tagger, corpus)
+trainer.train('/content/drive/MyDrive/thesis/resources/taggers/ner_gene_elmo', learning_rate=0.1, mini_batch_size=32,
+              max_epochs=20, embeddings_storage_mode='none')
